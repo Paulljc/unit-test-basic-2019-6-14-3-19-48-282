6ac2674e192c7d636d17d0e9e15904e320f6f814
GUKSON LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUGU2@OOCL.COM>
2019-07-15 15:08:01 +0800

    [LJC]: complete should_return internal expense type if project is internal

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -6,15 +6,21 @@ import ExpenseService.Project.Project;
 import ExpenseService.Project.ProjectType;
 import org.junit.jupiter.api.Test;
 
+import static ExpenseService.Expense.ExpenseType.INTERNAL_PROJECT_EXPENSE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.isEquals;
 
 class ExpenseServiceTest {
     @Test
     void should_return_internal_expense_type_if_project_is_internal() throws UnexpectedProjectTypeException {
         // given
+        Project project = new Project(ProjectType.INTERNAL, "project1");
         // when
+        ProjectType projectType = project.getProjectType();
         // then
+        assertThat(projectType).isEqualTo(INTERNAL_PROJECT_EXPENSE);
     }
 
     @Test
@@ -27,8 +33,11 @@ class ExpenseServiceTest {
     @Test
     void should_return_expense_type_B_if_project_is_external_and_name_is_project_B() throws UnexpectedProjectTypeException {
         // given
+
         // when
+
         // then
+
     }
 
     @Test
